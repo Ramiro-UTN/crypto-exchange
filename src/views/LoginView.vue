9841c4fe9dd347cf736d5fb3d3d2442c6f535d4c
ramiro <102697125+Ramiro-UTN@users.noreply.github.com>
2023-07-12 15:45:39 -0300

    Add LoginView and login route

---
+++ src/views/LoginView.vue
@@ -0,0 +1,77 @@
+<script setup>
+import axios from 'axios';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import Password from 'primevue/password';
+import { ref } from 'vue';
+import {useUsersStore} from '../stores/user'
+import { useRouter } from 'vue-router';
+
+const store = useUsersStore();
+const userId = ref(null);
+const password = ref(null);
+const {setUser} = store;
+const router = useRouter();
+
+const handleSubmit = async () => {
+  const userObject = {
+    userName: userId.value,
+    password: password.value
+  }
+
+  await login(userObject);
+}
+
+const login = async (values) => {
+  console.log('values:', values, "type:", typeof values);
+  const request = axios.post('http://localhost:3001/auth/login', values);
+  const loggedIn = await request.then(response => response.data);
+ 
+  if(loggedIn){
+    setUser(loggedIn);
+    // router.push("/transactions");
+  }
+  console.log('response:', loggedIn);
+  
+}
+
+
+</script>
+<template>
+  <form @submit.prevent="handleSubmit()">
+    <div class="label-input-group">
+      <label for="username">Username</label>
+      <span class="p-input-icon-left inputBox">
+        <i class="pi pi-user" />
+        <InputText id="username" v-model="userId" placeholder="Username" />
+      </span>
+    </div>
+    <div class="label-input-group">
+      <label for="password">Password</label>
+      <Password v-model="password" placeholder="Password" toggleMask id="password" />
+    </div>
+
+    <Button class="btn" type="submit" label="Sign In" icon="pi pi-check" aria-label="Sign In" />
+  </form>
+</template>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  place-items: center;
+  row-gap: 1rem;
+  padding: 2rem;
+  margin-top: 1rem;
+}
+
+.label-input-group {
+  display: flex;
+  flex-direction: column;
+
+}
+
+.btn {
+  width: 150px;
+}
+</style>
