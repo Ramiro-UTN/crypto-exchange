c7f03d266330a09af09d925c19235f8156d7f251
ramiro <102697125+Ramiro-UTN@users.noreply.github.com>
2023-07-25 23:35:36 -0300

    Add register function

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,47 +1,64 @@
 <script setup>
 import axios from 'axios';
+import { RouterLink } from 'vue-router';
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
 import Password from 'primevue/password';
+import Toast from 'primevue/toast';
+import { useToast } from "primevue/usetoast";
+const toast = useToast();
 import { ref } from 'vue';
 import { useUsersStore } from '../stores/user'
 import { useRouter } from 'vue-router';
 
+
+
 const store = useUsersStore();
 const userId = ref(null);
 const password = ref(null);
+const email = ref(null);
 const { setUser } = store;
 const router = useRouter();
 const loading = ref(false)
+const isLogin = ref(true);
+
+
 
 const handleSubmit = async () => {
-  const userObject = {
-    userName: userId.value,
-    password: password.value
-  }
+  const userObject = (isLogin.value) ? { userName: userId.value, password: password.value }
+  :{ userName: userId.value, email: email.value, password: password.value }
 
-  await login(userObject);
+  if (isLogin.value) await login(userObject);
+  if (!isLogin.value) await register(userObject);
 }
 
 const login = async (values) => {
-  loading.value = true;
+  //loading.value = true;
   console.log('values:', values, "type:", typeof values);
   const request = axios.post('http://localhost:3001/auth/login', values);
   const loggedIn = await request.then(response => response.data);
 
   if (loggedIn) {
     setUser(loggedIn);
-    loading.value = false;
     router.push("/");
   }
   console.log('response:', loggedIn);
+}
+const register = async (values) => {
 
+  const request = axios.post('http://localhost:3001/auth/register', values);
+  const savedUser = await request.then(response => response.data);
+  if(savedUser){
+    toast.add({ severity: "success", summary: 'Confirmed', detail: 'you have successfully registered', life: 3000 });
+    isLogin.value = true;
+  }
 
 }
 
 
 </script>
 <template>
+  <Toast/>
   <div class="cont">
     <form @submit.prevent="handleSubmit()">
       <div class="label-input-group">
@@ -51,13 +68,28 @@ const login = async (values) => {
           <InputText id="username" v-model="userId" placeholder="Username" />
         </span>
       </div>
+      <template v-if="!isLogin">
+        <div class="label-input-group">
+          <label for="email">Email</label>
+          <span class="p-input-icon-left inputBox">
+            <i class="pi pi-envelope" />
+            <InputText id="email" v-model="email" placeholder="Email" />
+          </span>
+        </div>
+      </template>
       <div class="label-input-group">
         <label for="password">Password</label>
         <Password v-model="password" placeholder="Password" toggleMask id="password" />
       </div>
 
-      <Button :loading="loading" class="btn" type="submit" label="Sign In" icon="pi pi-check" aria-label="Sign In" />
+      <Button class="btn" type="submit" :label="isLogin ? 'Sign In' : 'Sign Up'" icon="pi pi-check" />
+      <small>
+        <RouterLink @click="isLogin = !isLogin" :to="isLogin ? '/register' : '/login'">
+          {{ isLogin ? "Don't have an account? Sign Up" : "Already have an account? Sign In" }}
+        </RouterLink>
+      </small>
     </form>
+
   </div>
 </template>
 
